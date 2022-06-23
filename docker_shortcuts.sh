c4f97b4b5c16a16434149ca45bbce96d547e411b
Arthur Abeilice <afa7789@gmail.com>
2022-06-23 10:47:58 -0300

    Update docker_shortcuts.sh

--- docker_shortcuts.sh
+++ docker_shortcuts.sh
@@ -1,4 +1,8 @@
 docker rm --force $(docker ps -a -q)
 docker image rm $(docker image ls -q)
+docker volume rm $(docker volume ls -q)
+docker stop $(docker ps -a -q)
+docker kill $(docker ps -a -q)
+
 
 docker_networkIP() { \nNAME=$1;\ndocker ps | grep $NAME | awk '{print $1}'| docker inspect | grep "IPAddress";\n}
