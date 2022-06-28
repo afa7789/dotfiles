489cebb059ad87aaee683816130686864d5ecbe3
Arthur Abeilice <afa7789@gmail.com>
2022-06-28 16:52:10 -0300

    Update docker_shortcuts.sh

--- docker_shortcuts.sh
+++ docker_shortcuts.sh
@@ -6,3 +6,8 @@ docker kill $(docker ps -a -q)
 
 
 docker_networkIP() { \nNAME=$1;\ndocker ps | grep $NAME | awk '{print $1}'| docker inspect | grep "IPAddress";\n}
+
+// docker exec first one in a line
+taskid=$(docker service ps -q ${servicename} |head -1)
+containerid=$(docker inspect -f '{{.Status.ContainerStatus.ContainerID}}' ${taskid})
+docker exec -it ${containerid} "$*"
